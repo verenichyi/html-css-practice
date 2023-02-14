d1183dba8df90c7b87404d89b0db72c43119002d
verenichyi <verenichyi@gmail.com>
2023-02-14 16:00:37 +0300

    feat: implement inner header with menu

--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,15 @@
-import './styles.scss'; // DO NOT REMOVE!
-/* WRITE YOUR CODE BELOW */
+import './styles.scss';
+
+const menuButton = document.querySelector('.menu__button');
+
+if (menuButton) {
+  const menuBody = document.querySelector('.menu__body');
+  const menu = document.querySelector('.menu');
+
+  menuButton.addEventListener('click', () => {
+    document.body.classList.toggle('_lock');
+    menuButton.classList.toggle('menu__button_active');
+    menuBody.classList.toggle('menu__body_active');
+    menu.classList.toggle('menu_active');
+  });
+}
